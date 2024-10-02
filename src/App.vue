ea4978ba4cb687003b1c71646f8b4447f8e0cddf
qs_theme <qstxdy@gmail.com>
2024-10-02 15:33:38 +0800

    跨域？

--- src/App.vue
+++ src/App.vue
@@ -447,7 +447,6 @@ const sendMsgToService = () => {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
             },
             body: JSON.stringify({
                 model: model.value,
